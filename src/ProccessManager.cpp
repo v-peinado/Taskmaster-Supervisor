9709a1a70d3d9984374f8e6e408576e9daafdd83
v-peinado <peinado1985@gmail.com>
2026-06-13 21:56:55 +0200

    feat(proccess manager): add launch without exec

--- src/ProccessManager.cpp
+++ src/ProccessManager.cpp
@@ -1,7 +1,10 @@
 #include "ProccessManager.hpp"
 #include <sstream>
+#include "Logger.hpp"
+#include <unistd.h>
+#include <sys/types.h>
 
-// Constructors//destructors
+// Constructors//destructor
 
 ProccessManager::ProccessManager(Logger& logger) 
     : m_logger(logger) {}
@@ -27,7 +30,21 @@ void ProccessManager::startManager(const std::vector<ProgramConfig>& configs) {
 void ProccessManager::launch(Program& program) {
     const ProgramConfig& cfg = program.getProgramConfig();
     std::vector<std::string> args = splitCmd(cfg.cmd);
-    
+    if(args.empty()) {
+        m_logger.log(Logger::LogLevel::Error, "Empty cmd for " + cfg.name);
+        program.setFatalError();
+        return;
+    }
+    pid_t pid = fork();
+    if(pid < 0) {
+        m_logger.log(Logger::LogLevel::Error, "for failed for " + cfg.name);
+        program.setFatalError();
+        return;       
+    }
+    if(pid == 0) {
+        // setup chilkd and exec
+    }
+    program.started(pid);
 }
 
 
