91a0e840ec1befd4d757f0fb67bca64d956b6b92
v-peinado <peinado1985@gmail.com>
2026-08-18 21:27:57 +0200

    feat: add ClientConnection with line buffering

---
+++ include/ClientConnection.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "Fd.hpp"
+#include <string>
+#include <vector>
+
+class ClientConnection {
+    public:
+
+        ClientConnection(Fd fd);
+        ClientConnection() = delete;
+        ~ClientConnection() = default;
+        ClientConnection(const ClientConnection&) = delete;
+        ClientConnection& operator=(const ClientConnection&) = delete;
+        ClientConnection(ClientConnection&&) = default;
+        ClientConnection& operator=(ClientConnection&&) = default;
+
+    private:
+
+        Fd          m_fd;
+        std::string m_buffer;
+        bool        m_closed;
+};
